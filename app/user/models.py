db1b03a4b1cd33e61c94d659bae0db51da933bb2
Fedorov <atiliogoulart@gmail.com>
2021-09-29 18:50:20 -0300

    Add user's model

--- app/user/models.py
+++ app/user/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class UserModel(models.Model):
+    
+    name = models.CharField('Name', max_length=50)
+    age = models.PositiveBigIntegerField('Age', default=18)
+
+    def __str__(self):
+        return self.name
