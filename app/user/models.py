b385be0f7aec25a5a0ede98d9bebf979ee9968d1
Hector <atilioprofissional@gmail.com>
2021-09-30 22:15:48 -0300

    Changing the models

--- app/user/models.py
+++ app/user/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
 class UserModel(models.Model):
+
+    STATUS_OPCOES = (
+        ('DRAFT','DRAFT'),
+        ('TRASH','TRASH'),
+        ('PUBLISHED','PUBLISHED'),
+    )
     
     name = models.CharField('Name', max_length=50)
+    imported_t = models.DateTimeField(auto_now=True)
+    status = models.CharField('Status', choices=STATUS_OPCOES , default="DRAFT")
     age = models.PositiveBigIntegerField('Age', default=18)
 
     def __str__(self):
