c84cc5b9cec72e7366ec67444c8fcb8e7c1a91c4
Fedorov <atiliogoulart@gmail.com>
2021-10-03 18:12:18 -0300

    Update auth setup

--- app/user/api.py
+++ app/user/api.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.permissions import DjangoModelPermissions
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import IsAuthenticated
 
@@ -10,6 +10,8 @@ from .serializer import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = UserModel.objects.all()
     serializer_class = UserSerializer
+    authentication_classes = [SessionAuthentication, BasicAuthentication]
+    permission_classes = [IsAuthenticated]
     filter_backends = (SearchFilter, DjangoFilterBackend, OrderingFilter)
     search_fields = (
         "imported_t",
