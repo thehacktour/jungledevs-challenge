b9dbe4b04be663c681f397617cff3278094f3e7a
Fedorov <atiliogoulart@gmail.com>
2021-10-03 16:06:52 -0300

    Add permissions into API

--- app/user/api.py
+++ app/user/api.py
@@ -1,4 +1,7 @@
 from rest_framework import viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.permissions import DjangoModelPermissions
+from rest_framework.filters import SearchFilter, OrderingFilter
 
 from .models import UserModel
 from .serializer import UserSerializer
@@ -6,3 +9,4 @@ from .serializer import UserSerializer
 class UserViewSet(viewsets.ModelViewSet):
     queryset = UserModel.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (DjangoModelPermissions,)
