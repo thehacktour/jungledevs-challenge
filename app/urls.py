3c821642e5778266933320436dd12b16f808fe73
Fedorov <atiliogoulart@gmail.com>
2021-10-03 17:14:07 -0300

    Add API swagger

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from django.views.generic import TemplateView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+
+    path('swagger-ui/', TemplateView.as_view(
+        template_name='swagger-ui.html',
+        extra_context={'schema_url': 'openapi-schema'}
+    ), name='swagger-ui'),
 
     path('api/oficial/', include('app.user.urls')),
 ]
