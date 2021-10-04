a3d5e03bbff479d229b9c4a60b2a6a9a2e85529c
Fedorov <atiliogoulart@gmail.com>
2021-10-04 13:18:48 -0300

    More updates

--- app/user/serializer.py
+++ app/user/serializer.py
@@ -8,6 +8,7 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = UserModel
         fields = (
+            "pk",
             "imported_t",
             "status",
             "gender",
