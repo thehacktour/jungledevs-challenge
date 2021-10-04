b1a12980be21080e4fd8df4e8eea42e0a5b8364a
Fedorov <atiliogoulart@gmail.com>
2021-10-04 17:02:34 -0300

    Add update into endpoints

--- app/user/api.py
+++ app/user/api.py
@@ -3,6 +3,9 @@ from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.authentication import SessionAuthentication, BasicAuthentication
 from rest_framework.filters import SearchFilter, OrderingFilter
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from app.user import mensagens
+from rest_framework import status
 
 from .models import UserModel
 from .serializer import UserSerializer, UserSerializerExpand
@@ -33,6 +36,23 @@ class UserViewSet(viewsets.ModelViewSet):
         "uuid",
         "username",
     )
+
+    def create(self, request, *args, **kwargs):
+        try:
+            return super(UserViewSet, self).create(request, *args, **kwargs)
+        except KeyError:
+            return Response(
+                {"non_field_errors": mensagens.MSG_ERRO},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+    def destroy(self, request, pk, *args, **kwargs):
+        try:
+            UserModel.objects.filter(id=pk).update()
+            return Response({"detail": mensagens.MSG5}, status=status.HTTP_200_OK)
+        except Exception:
+            return Response(
+                {"detail": mensagens.MSG_ERRO}, status=status.HTTP_400_BAD_REQUEST
+            )
 
 class UserExpandableViewSet(viewsets.ModelViewSet):
     queryset = UserModel.objects.all()
