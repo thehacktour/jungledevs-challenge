f50563f87f29f383bae8dd8b8394c1edcb270e5b
Fedorov <atiliogoulart@gmail.com>
2021-10-04 16:49:06 -0300

    New endpoint

--- app/user/urls.py
+++ app/user/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
 
-from .api import UserViewSet
+from .api import UserViewSet, UserExpandableViewSet
 
 router = DefaultRouter()
 
 router.register('user', UserViewSet, basename='user')
+router.register('user-crazy', UserExpandableViewSet, basename='user-crazy')
 
 urlpatterns = router.urls
