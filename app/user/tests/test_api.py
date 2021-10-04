e593320599fd1b8df07888801533afe2be1f704a
Fedorov <atiliogoulart@gmail.com>
2021-10-04 14:27:28 -0300

    Add some unit tests

--- app/user/tests/test_api.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-
-class ApiTestCase(TestCase):
-    def test_inicial(self):
-        response = self.client.get('http://localhost:8000/')
-        self.assertEqual(response.status_code, 200)
