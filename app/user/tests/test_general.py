5371006efb2ed12d811677f11efca74a137ff2a2
Atilio <atilio.jesus@grupoboticario.com.br>
2022-06-17 22:44:24 -0300

    Update travis.yml

--- app/user/tests/test_general.py
+++ app/user/tests/test_general.py
@@ -1,6 +1,2 @@
 from django.test import TestCase
 from rest_framework import status
-
-def test_inicial(self):
-    response = self.client.get('http://localhost:8000/api/oficial/user/')
-    self.assertEqual(response.status_code, 200)
