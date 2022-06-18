e744e92fcc257e1c1408ed94751b2b7a9683f43c
Atilio <atilio.jesus@grupoboticario.com.br>
2022-06-17 22:41:43 -0300

    Update travis.yml

--- app/user/tests/test_general.py
+++ app/user/tests/test_general.py
@@ -1,7 +1,6 @@
 from django.test import TestCase
 from rest_framework import status
 
-class ApiTestCase(TestCase):
-    def test_inicial(self):
-        response = self.client.get('http://localhost:8000/api/oficial/user/')
-        self.assertEqual(response.status_code, 200)
+def test_inicial(self):
+    response = self.client.get('http://localhost:8000/api/oficial/user/')
+    self.assertEqual(response.status_code, 200)
