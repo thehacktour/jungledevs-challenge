bcf5dc9322e1289ba01864e132c05b5c9136dead
Atilio <atilio.jesus@grupoboticario.com.br>
2022-06-19 10:07:47 -0300

    Update UserModel

--- app/user/models.py
+++ app/user/models.py
@@ -3,9 +3,8 @@ import uuid
 
 class UserModel(models.Model):
     uuid = models.UUIDField(primary_key=True, unique=True, default=uuid.uuid4, editable=False)
-    username = models.CharField('Username', max_length=50)
+    email = models.EmailField('Email', max_length=100, default="")
     password = models.CharField('Password', max_length=50)
-    points = models.PositiveIntegerField(default=10)
 
     def __str__(self):
         return self.username
